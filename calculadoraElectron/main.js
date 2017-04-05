779d431e47d13d59cb8248057f9d13f49b63e636
adriansandoval <adrian.sandoval12345@gmail.com>
2017-04-05 15:02:14 -0600

    Calculadora Electron

---
+++ calculadoraElectron/main.js
@@ -0,0 +1,23 @@
+//const app  = requiere('electron').app
+//const BrowserWindow = require('electron').BrowserWindow
+const {app,BrowserWindow} = require('electron')
+//Ruta donde se encuentra nuestro proyecto
+const path = require('path')
+//Ruta pero en formato URL
+const url = require('url')
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	PantallaPrincipal = new BrowserWindow({
+		width: 1024, 
+		height : 768
+	})
+	PantallaPrincipal.on('closed',function(){
+		PantallaPrincipal = null
+	})
+	PantallaPrincipal.loadURL('http://www.marca.com')
+	PantallaPrincipal.show()
+}
+
+//La aplicación ejecuta este event cuando el archivo main.js se carga en memoria.
+app.on('ready', muestraPantallaPrincipal)
