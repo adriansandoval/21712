cb6644ea85f3d823b897b5bde0b9f4ee1384a0e6
adriansandoval <adrian.sandoval12345@gmail.com>
2017-04-26 14:58:42 -0600

    Agregar informacion de randomuser

--- randomuser/index.js
+++ randomuser/index.js
@@ -8,7 +8,12 @@ function datosRandom(){
 	  url: 'https://randomuser.me/api/',
 	  dataType: 'json',
 	  success: function(data) {
+	  	$("#txtGender").html(data.results[0].gender)
 	    $("#txtNombre").html(data.results[0].name.first + " " + data.results[0].name.last)
+	    $("#txtLocation").html(data.results[0].location.street + ", " + data.results[0].location.city + ", " + data.results[0].location.state + ", " + data.results[0].location.postcode)
+	    $("#txtEmail").html(data.results[0].email)
+	    $("#txtDob").html(data.results[0].dob)
+	    $("#txtRegistered").html(data.results[0].registered)
 	    $("#imgFoto").attr("src", data.results[0].picture.large)
 	  },
 	  error: function(a,b,c) {
