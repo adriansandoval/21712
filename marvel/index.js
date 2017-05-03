eee4b79b3a931ad53c8ef8540085daef02734c98
adriansandoval <adrian.sandoval12345@gmail.com>
2017-05-03 14:59:40 -0600

    Convertir a PDF

--- marvel/index.js
+++ marvel/index.js
@@ -1,8 +1,65 @@
-const rq = require('electron-require');
+const rq   = require('electron-require');
 const main = rq.remote('./main.js');
-const $ = require("jquery");
+const $    = require('jquery');
+//Constantes para imprimir en PDF (ventana que imprimimos)
+const ipc  = require('electron').ipcRenderer
+const btnPDF = document.getElementById('btnPDF')
+btnPDF.addEventListener('click', function(event){
+	ipc.send('print-to-pdf')
+})
 
+//http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=
+var buscarPersonaje = function(){
+	var personaje = $("#txtPersonaje").val();
+	var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=";
+	var cantidadComics = 0;
+	var cadenaComics   = "";
+	if(personaje == ""){
+		alert("Ingrese el personaje")
+		$("#txtPersonaje").focus();
+		return//Ya no continua con la función
+	}
 
-$('#txtPersonaje').focus();
+	url = url+personaje;
+	$.ajax({
+		beforeSend: function(){
+			$("#imgLoader").show();
+		},
+		dataType: "json",
+		url: url,
+		success:function(response){
+			$("#imgLoader").hide();
+			if(response.code==200){//ok.cat
+				$("#imgFoto").show("slow");
+				$("#imgFoto").attr("src",response.data.results[0].thumbnail.path+"."+
+					  					response.data.results[0].thumbnail.extension)
+				$("#txtNombre").html(response.data.results[0].name);
+				$("#txtDescripcion").html("")
+				//Validamos si hay o no descripcion del personaje
+				if(response.data.results[0].description!=""){
+					$("#txtDescripcion").html("<b>Descripción: </b>"+response.data.results[0].description)
+				}
+				cantidadComics=response.data.results[0].comics.returned;
+				for(var i=0;i<cantidadComics;i++){
+					cadenaComics+=response.data.results[0].comics.items[i].name+"<br>"
+				}
+				$("#txtComics").html(cadenaComics);
+			}
+		}
+	})
+	$("#txtPersonaje").val("");//vaciamos la caja de texto
+	$("#txtPersonaje").focus();
+}
 
-$('#btnBuscar').on('click', buscarPersonaje);
+var teclaPersonaje = function(tecla){
+	if(tecla.which == 13){
+		buscarPersonaje();
+	}
+}
+
+//Posiciona el cursor en el cuadro de texto
+$("#txtPersonaje").focus();
+//Activar las teclas que se presionan en el cuadro de texto
+$("#txtPersonaje").on("keypress",teclaPersonaje);
+//Evento del botón btnBuscar-click
+$("#btnBuscar").on("click",buscarPersonaje);
--- marvel/main.js
+++ marvel/main.js
@@ -1,7 +1,30 @@
 const {app,BrowserWindow} = require('electron')
+const electron = require('electron')
 const path = require('path')
 const url = require('url')
 let PantallaPrincipal;
+//Constantes para impresión en PDF
+const fs = require('fs')//Sistema de archivos
+const os = require('os')//Sistema Operativo
+//Llamada al procedimiento interno
+const ipc = electron.ipcMain
+//Interfaz de comandos(terminal)
+const shell = electron.shell
+
+//Evento para Imprimir en PDF
+ipc.on('print-to-pdf', function(event){
+	const pdfPath = path.join(os.tmpdir(), 'print.pdf')
+	const win = BrowserWindow.fromWebContents(event.sender)
+	win.fromWebContents.printToPDF({},function(error,data){
+		if(error)
+			throw error;
+		fs.writeFile(pdfPath, data, function(error){
+			if(error)
+				throw error;
+			shell.openExternal('file://' + pdfPath)
+		})
+	})
+})
 
 function muestraPantallaPrincipal(){
 	PantallaPrincipal = new BrowserWindow({
@@ -17,6 +40,7 @@ function muestraPantallaPrincipal(){
 		protocol: 'file',
 		slashes: true
 	}))
+	PantallaPrincipal.webContents.openDevTools()
 	PantallaPrincipal.show()
 }
 
